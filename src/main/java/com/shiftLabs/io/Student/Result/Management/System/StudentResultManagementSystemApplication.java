c61510dbf3deb06fe7427314d42d2960ca36fb6d
flaviocr2012 <flaviocr2012@gmail.com>
2023-09-29 14:35:22 -0300

    initial config: pom + dataBase

---
+++ src/main/java/com/shiftLabs/io/Student/Result/Management/System/StudentResultManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.shiftLabs.io.Student.Result.Management.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentResultManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentResultManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shiftLabs/io/Student/Result/Management/System/StudentResultManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiftLabs.io.Student.Result.Management.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentResultManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
